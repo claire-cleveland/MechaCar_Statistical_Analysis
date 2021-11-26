27f7fe1c2a15e96029acc02ba4dec0a8baa3023f
Claire-Cleveland <Claire.Clebeland@gmail.com>
2021-11-25 19:54:04 -0700

    Update

---
+++ MechaCarChallenge.R
@@ -0,0 +1,26 @@
+# import library
+library(dplyr)
+
+
+#demo_table <- read.csv(file='demo.csv', check.names=F, stringsAsFactors = F)
+mpg_data <- read.csv(file='Resources/MechaCar_mpg.csv', check.names=F, stringsAsFactors = F)
+
+
+lmResult = lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data =mpg_data)
+summary(lmResult)
+
+coil_Data <- read.csv('Resources/Suspension_Coil.csv')
+
+total_summary <- coil_Data %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), Standard_dev=sd(PSI))
+total_summary
+
+lot_summary <- coil_Data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), Standard_dev=sd(PSI), .groups='keep')
+lot_summary
+
+?t.test()
+
+t.test(log10(lot_summary$Mean),mu=mean(log10(total_summary$Mean))) #compare sample versus population means
+
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot1")$PSI, mu=1500)
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot2")$PSI, mu=1500)
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot3")$PSI, mu=1500)
